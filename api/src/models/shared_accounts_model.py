a8acb21d80361def3c321db18d65b6d47446722c
marquinhoDaCP <marcus.eevil1@gmail.com>
2026-05-05 12:33:41 -0400

    fix: add id_owner column and rename id_owner column to id_account

--- api/src/models/shared_accounts_model.py
+++ api/src/models/shared_accounts_model.py
@@ -5,7 +5,8 @@ class SharedAccountsModel(Base):
     __tablename__ = 'shared_accounts'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    id_owner_account = Column(Integer, ForeignKey('user_accounts.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
+    id_owner = Column(Integer, ForeignKey('users.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
     id_member = Column(Integer, ForeignKey('users.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
+    id_account = Column(Integer, ForeignKey('user_accounts.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
     can_view_balance = Column(SmallInteger(), default=0, nullable=False)
     can_view_transaction = Column(SmallInteger(), default=0, nullable=False)
