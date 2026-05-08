264a324cf54f7063b6814251a1158f8f7a32d87a
marquinhoDaCP <marcus.eevil1@gmail.com>
2026-05-08 18:25:53 -0400

    feat: add get_api_key method

--- api/src/services/pluggy_service.py
+++ api/src/services/pluggy_service.py
@@ -1,13 +1,27 @@
 import httpx
+from ..config.config_api import (
+    PLUGGY_API_URL,
+    PLUGGY_CLIENT_ID,
+    PLUGGY_CLIENT_SECRET
+)
 from ..middlewares.utils import HOURS_IN_SECONDS
 
 class PluggyService:
     def __init__(self):
         self.__apiKey: str = None # Stores API Key in cache
-        self.__expiresAtInSeconds: int = 2 * HOURS_IN_SECONDS
+        self.__expiresAt: int = 2 * HOURS_IN_SECONDS # Expires in seconds (2h)
 
     async def get_api_key(self) -> str:
-
-        is_token_expired =
+        if not self.__apiKey:
+            async with httpx.AsyncClient() as http:
+                response = await http.post(
+                    PLUGGY_API_URL + '/auth',
+                    json={
+                        "clientId": PLUGGY_CLIENT_ID,
+                        "clientSecret": PLUGGY_CLIENT_SECRET
+                    }
+                )
+            
+            response.raise_for_status() 
 
 pluggy_service = PluggyService()
